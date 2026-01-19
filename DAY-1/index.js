a49244640ee864c7d35a760d85b0f09e909e6524
naitik-cmd <naitikchs16@gmail.com>
2026-01-19 12:34:23 +0530

    how to use packages and access its functionalities

--- DAY-1/index.js
+++ DAY-1/index.js
@@ -1,4 +1,4 @@
-console.log('Hi, I am Naitik. And I am the best Full-stack developer.');
-for(let i=0; i<10; i++){
-    console.log(i);
-}
+// 
+
+const catMe= require("Cat-me");
+console.log(catMe());
