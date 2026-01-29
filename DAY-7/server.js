073d0355b239b1a2deac3a36812f2b177402a5b7
naitik-cmd <naitikchs16@gmail.com>
2026-01-30 02:01:10 +0530

    Databasse connected successfully, last class revision done right.

--- DAY-7/server.js
+++ DAY-7/server.js
@@ -1,1 +1,17 @@
 //-server ko start krna aur database connect krna :))
+
+const app= require('./src/app');
+const mongoose = require("mongoose");
+
+const connectToDb= ()=>{
+    mongoose.connect('mongodb+srv://naitik:pZsi7Vq4YVjtO3f7@cluster0.vftlhyz.mongodb.net/class1')
+    .then(()=>{
+        console.log('Databasse connected successfully!');
+    })
+}
+
+connectToDb(); //connection to database :)) function call
+
+app.listen(3000,()=>{
+    console.log("Server is running at port 3000.");
+})
--- DAY-7/src/app.js
+++ DAY-7/src/app.js
@@ -1,1 +1,7 @@
 //-server ko create and config krna
+
+const express= require("express");
+
+const app = express(); //instance of the server created
+
+module.exports= app;
