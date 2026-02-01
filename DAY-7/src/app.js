435110206445bf5ddf197511719956c2ff2dcac8
naitik-cmd <naitikchs16@gmail.com>
2026-02-01 22:57:01 +0530

    Understanding CRUD operations

--- DAY-7/src/app.js
+++ DAY-7/src/app.js
@@ -1,7 +1,26 @@
 //-server ko create and config krna
 
 const express= require("express");
-
+const noteModel= require("./models/notes.model");
 const app = express(); //instance of the server created
+
+app.use(express.json()); //Middleware
+
+//POST API (req.body)
+
+app.post('/notes',async (req,res)=>{
+    const {title,description}= req.body;
+
+    const note= await noteModel.create({
+        title,description
+    })
+
+    res.status(201).json({
+        message: "Note created successfull!",
+        note
+    })
+})
+
+
 
 module.exports= app;
