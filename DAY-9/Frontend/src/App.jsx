b829e800fadee38f11ae15c0d3898ae7769ab352
naitik-cmd <naitikchs16@gmail.com>
2026-02-06 22:46:55 +0530

    Added get, post and delete API from frontend

--- DAY-9/Frontend/src/App.jsx
+++ DAY-9/Frontend/src/App.jsx
@@ -1,37 +1,83 @@
 import React from 'react'
 import { useState } from 'react'
 import axios from "axios"
+import { useEffect } from 'react';
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      title: "test title 1",
-      descripition: "test description 1"
-    },
-    {
-      title: "test title 1",
-      descripition: "test description 1"
-    },
-    {
-      title: "test title 1",
-      descripition: "test description 1"
-    },
-    {
-      title: "test title 1",
-      descripition: "test description 1"
-    }
-  ]);
-  axios.get('http://localhost:3000/api/notes')
-  .then((res)=>{
-    setNotes(res.data.notes);
-  })
+
+  const [notes, setNotes] = useState([]);
+  console.log("Hello integration!")
+
+  function fetchNotes(){
+      axios.get('http://localhost:3000/api/notes')
+      .then((res)=>{
+      setNotes(res.data.notes); 
+      })
+  }
+  
+  useEffect(()=>{
+    fetchNotes();
+  },[])
+
+  function handleSubmit(e){
+    e.preventDefault() //page ko reload hone se rokega
+
+    const {title,description}= e.target.elements //html tag of twon input tags
+
+    console.log(title.value,description.value);
+    
+    //Creating notes at the server from frontend using axios.
+    axios.post('http://localhost:3000/api/notes', {
+      title: title.value,
+      description: description.value
+    })
+    .then((res)=>{
+      console.log(res.data);
+      fetchNotes();
+    })
+
+  }
+
+  //Function for Deleting any note.
+
+  function handleDeleteNote(noteId){
+    console.log(noteId);
+    axios.delete("http://localhost:3000/api/notes/"+noteId)
+    .then((res)=>{
+      console.log(res.data);
+      fetchNotes()
+    })
+  }
+
+  function handleUpdateNote(noteId){
+    console.log(noteId);
+    axios.patch('http://localhost:3000/api/notes/'+noteId,{
+      description: Modified
+    })
+    .then((res)=>{
+      console.log(res.data);
+      fetchNotes();
+    })
+  }
+
+
   return (
     <>
+
+    <form className='note-create-form' onSubmit={handleSubmit}>
+      <input name='title' type="text" placeholder='Enter title' />
+      <input name='description' type="text" placeholder='Enter description' />
+      <button>Create note</button>
+    </form>
+
+
     <div className="notes">
       {
         notes.map((note,id)=>{
         return <div key={id} className="note">
                   <h1>{note.title}</h1>
                   <p>{note.description}</p>
+                  <button className='delete' onClick={()=>{handleDeleteNote(note._id)}}>Delete</button>
+                  <button className='update'onClick={()=>{handleUpdateNote(note._id)}}>Update</button>
                </div>
         })
       }
