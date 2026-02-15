4999c52dabce305609605f4e4206fb2f5c7506e6
naitik-cmd <naitikchs16@gmail.com>
2026-02-15 23:31:21 +0530

    Insta user model created

---
+++ DAY-14-insta/src/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose= require("mongoose");
+
+const userSchema= new userSchema({
+    username: {
+        type:String,
+        unique:[true,"Username is already taken."],
+        required:[true,"User name is required."]
+    },
+    password: {
+        type:String,
+        required:[true,"Password is required."]
+    },
+    email: {
+        type:String,
+        unique:[true,"Email already exists."],
+        required:[true,"Email is required."]
+    },
+    bio: {
+        type:String    
+    },
+    profile_image: {
+        type:String,
+        // default:
+    }
+})
