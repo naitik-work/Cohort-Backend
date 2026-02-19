afd56e458ca33bec53aeb343f1dbc458885cb3f8
naitik-cmd <naitikchs16@gmail.com>
2026-02-19 19:11:26 +0530

    getPostController and getPostDetailsController created

--- DAY-14-insta/src/controllers/post.controller.js
+++ DAY-14-insta/src/controllers/post.controller.js
@@ -49,6 +49,85 @@ async function createPostController(req,res){
     })
 }
 
+async function getPostController(req,res){
+     const token= req.cookies.token;
+
+    if(!token){
+        res.status(401).json({
+            message: "Unauthorized access!"
+        })
+    }
+
+     let decoded= null;
+     try{
+        decoded= jwt.verify(token, process.env.JWT_SECRET);
+     }
+     catch(err){
+        return res.status(401).json({
+            message: "Token invalid"
+        })
+     }
+
+     const userId= decoded.id;
+      
+     const posts= await postModel.find(
+        {user: userId}
+     )
+
+     res.status(200).json({
+        "message": "Posts fetched successfully.",
+         posts
+     })
+}
+
+async function getPostDetailsController(){
+    const token= req.cookies.token;
+
+    if(!token){
+        res.status(401).json({
+            message: "Unauthorized access!"
+        })
+    }
+
+    let decoded= null
+
+    try{
+        decoded= jwt.verify(token, process.env.JWT_SECRET);
+    }
+    catch(err){
+        res.send(401).json({
+            message: "token invalid."
+        })
+    }
+    const userId= decoded.id;
+    const postId= req.params.postId;
+
+    const post= await postModel.findById({postId});
+
+    if(!post){
+        res.status(404).json({
+            message: "Post not found."
+        })
+    }
+
+    const isValidUser = post.user.toString()===userId;
+
+    if (!isValidUser) {
+        return res.status(403).json({
+            message: "Forbidden Content."
+        })
+    }
+
+    return res.status(200).json({
+    message: "Post fetched  successfully.",
+    post
+    })
+
+}
+
+
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetailsController
 }
--- DAY-14-insta/src/routes/post.routes.js
+++ DAY-14-insta/src/routes/post.routes.js
@@ -4,9 +4,18 @@ const upload= multer({ storage: multer.memoryStorage()})
 const postRouter= express.Router();
 
 const postController= require("../controllers/post.controller")
+
+
 //POST /api/posts [protected]
 //- req.body = { caption,image-file }
-
 postRouter.post('/',upload.single("image"),postController.createPostController)
 
+//GET /api/posts/ [protected]
+postRouter.get('/',postController.getPostController);
+
+//GET /api/posts/details/:postId
+//-> return an detail about specific post with the id. Also check whether the post 
+//belongs to the user that the request come from.
+
+postRouter.get('/details/:postId', postRouter.getPostDetailsController);
 module.exports= postRouter
