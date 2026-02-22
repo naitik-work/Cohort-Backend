f48f6e76bf7ebe31bc73d1168b06f8a0fa4d11e6
naitik-cmd <naitikchs16@gmail.com>
2026-02-22 23:51:08 +0530

    Implement login functionality and create auth API service

--- Insta-clone/frontend/src/features/auth/pages/Login.jsx
+++ Insta-clone/frontend/src/features/auth/pages/Login.jsx
@@ -10,15 +10,7 @@ const Login = () => {
     async function handleSubmit(e) {
         e.preventDefault();
 
-        axios.post("http://localhost:3000/api/auth/login", {
-            username,
-            password
-        },{
-            withCredentials: true
-        })
-        .then((res)=>{
-            console.log(res.data);
-        })
+
     }
 
   return (
